f957ec4783ee5805d10788dfa7b6f56dfd360cf4
Michael Arnott <arnottmj@aol.com>
2015-07-29 10:51:51 +0100

    first commit

---
+++ js/gitUserSearchController.js
@@ -0,0 +1,9 @@
+githubUserSearch.controller('GitUserSearchController', ['$resource', function($resource) {
+  var self = this;
+  var searchResource = $resource('https://api.github.com/search/users?access_token=' + access_token);
+  self.doSearch = function() {
+    self.searchResult = searchResource.get(
+      { q: self.searchTerm }
+    );
+  };
+}]);
